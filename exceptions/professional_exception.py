c7e9de6fee235a0e79a394106ce94c1a316dacf2
Francisco Antonelli <franciscoantonelli1999@gmail.com>
2026-08-19 15:27:25 -0300

    Eliminando excepciones viejas y modificacion appointment schema

--- exceptions/professional_exception.py
+++
@@ -1,2 +0,0 @@
-class ProfessionalException(Exception):
-    pass
--- src/schemas/appointment_request.py
+++ src/schemas/appointment_request.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel # sirve para validar datos automaticamente, convertir json a objetos python
 from datetime import datetime
 
-class AppointmentRequest(BaseModel):
+#schema de entrada que fastapi usa para validar los datos que le llegan de la peticion
+
+class AppointmentRequest(BaseModel): #esta clase representa datos que vienen de una peticion
     professional_id: int
     client_id: int
     datetime_slot: datetime
