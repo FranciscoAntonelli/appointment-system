cd8a0466f2e84eb6b0b4a5cc3cbffe94ca0cf528
Francisco Antonelli <franciscoantonelli1999@gmail.com>
2026-08-25 22:26:57 -0300

    [200~Actualizando la configuracion de la aplicacion

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from src.routers.appointment_router import router as appointment_router
 from src.routers.professional_router import router as professional_router
-
+from src.routers.client_router import router as client_router
 # uvicorn src.main:app --reload
 
 app = FastAPI() #crea una app web
 
 app.include_router(appointment_router) #le registra al objeto app todas las rutas definidas en appointment_router
 app.include_router(professional_router)
+app.include_router(client_router)
