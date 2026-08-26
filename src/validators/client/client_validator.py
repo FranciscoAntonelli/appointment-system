de0024bb35006ef33278ac1dd0b7139c4554509e
Francisco Antonelli <franciscoantonelli1999@gmail.com>
2026-08-25 22:39:05 -0300

    Modificando los validadores de cliente y profesional

--- src/validators/client/client_validator.py
+++ src/validators/client/client_validator.py
@@ -1,4 +1,5 @@
-from src.exceptions.client_exception import ClientException
+
+from src.exceptions.validation_exception import ValidationException
 from src.validators.validator import Validator
 
 
@@ -11,7 +12,7 @@ class ClientValidator(Validator):
 
     def _validate_is_empty(self, data, field_name):
         if not data:
-             raise ClientException(f"El {field_name} está vacio")
+             raise ValidationException(f"El {field_name} está vacio")
         
     def _validate_email(self, email):
         count_at = 0
@@ -24,32 +25,32 @@ class ClientValidator(Validator):
 
         
         if count_at != 1:
-            raise ClientException("Tiene que tener exactamente un @")
+            raise ValidationException("Tiene que tener exactamente un @")
 
         if at_position == 0:
-            raise ClientException("Tiene que haber texto antes del @")
+            raise ValidationException("Tiene que haber texto antes del @")
 
         if at_position == len(email) - 1:
-            raise ClientException("Tiene que haber un dominio despues del @")
+            raise ValidationException("Tiene que haber un dominio despues del @")
         
         point_position = 0
 
         for pos in range(at_position + 1, len(email)):
             if email[pos] == ".":
                 if pos + 1 < len(email) and email[pos + 1] == ".":
-                    raise ClientException("No puede haber dos puntos seguidos en el dominio")
+                    raise ValidationException("No puede haber dos puntos seguidos en el dominio")
                 
                 point_position = pos
                 break
 
         if point_position == 0:
-            raise ClientException("El dominio tiene que tener un punto")
+            raise ValidationException("El dominio tiene que tener un punto")
 
         if point_position == at_position + 1:
-            raise ClientException("Tiene que haber texto entre el @ y el punto")
+            raise ValidationException("Tiene que haber texto entre el @ y el punto")
 
         if point_position == len(email) - 1:
-            raise ClientException("Tiene que haber texto despues del punto")
+            raise ValidationException("Tiene que haber texto despues del punto")
 
         
 
--- src/validators/professional/professional_validator.py
+++ src/validators/professional/professional_validator.py
@@ -1,4 +1,4 @@
-from src.exceptions.professional_exception import ProfessionalException
+from src.exceptions.validation_exception import ValidationException
 from src.validators.validator import Validator
 
 class ProfessionalValidator(Validator):
@@ -13,16 +13,16 @@ class ProfessionalValidator(Validator):
 
     def _validate_name(self, name):
         if not name:
-            raise ProfessionalException("No hay nombre")
+            raise ValidationException("No hay nombre")
 
     def _validate_duration(self, duration):
         try:
             number = int(duration)
         except ValueError:
-            raise ProfessionalException("No es un numero entero")
+            raise ValidationException("La duración no es un número entero")
 
         if number <= 0:
-            raise ProfessionalException("Debe ser mayor a 0")
+            raise ValidationException("La duración debe ser mayor a 0")
             
         if number > 480:
-            raise ProfessionalException("No puede superar los 480 minutos (8 horas)")
+            raise ValidationException("La duración no puede superar los 480 minutos (8 horas)")
