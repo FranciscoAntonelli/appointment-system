fad1af11b5bc045e5bf3074be1653090481293ef
Francisco Antonelli <franciscoantonelli1999@gmail.com>
2026-08-25 22:33:37 -0300

    Modificando el repositorio de cliente y el router

--- src/repositories/client/postgres_client_repository.py
+++ src/repositories/client/postgres_client_repository.py
@@ -1,6 +1,7 @@
+from src.exceptions.already_exists_exception import AlreadyExistsException
 from src.repositories.client.client_repository import ClientRepository
 from src.entities.client import Client
-
+from psycopg2.errors import UniqueViolation
 
 class PostgresClientRepository(ClientRepository):
 
@@ -30,6 +31,41 @@ class PostgresClientRepository(ClientRepository):
                 return None
             
             return Client(id=row[0], name=row[1], email=row[2], phone=row[3])
+
+        finally:
+            if cursor:
+                cursor.close()
+
+
+    def save(self, client):
+        cursor = None
+
+        try:
+
+            cursor = self._connection.cursor()
+
+            cursor.execute(
+                """
+                INSERT INTO clients (name, email, phone)
+                VALUES (%s, %s, %s)
+                RETURNING id
+                """,
+                (client.name, client.email, client.phone)
+            )
+
+            client_id = cursor.fetchone()[0]
+
+            self._connection.commit()
+
+            return client_id
+
+        except UniqueViolation:
+            self._connection.rollback()
+            raise AlreadyExistsException("El email ya está registrado")
+
+        except Exception:
+            self._connection.rollback()
+            raise
 
         finally:
             if cursor:
--- src/routers/client_router.py
+++ src/routers/client_router.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
+from src.exceptions.not_found_exception import NotFoundException
+from src.entities.client import Client
+from src.exceptions.validation_exception import ValidationException
+from src.exceptions.already_exists_exception import AlreadyExistsException
 from src.schemas.client_request import ClientRequest
 from src.dependencies.dependency_client_service import get_client_service
 
@@ -11,6 +15,26 @@ def create_client(request: ClientRequest, service = Depends(get_client_service))
     
     try:
 
-        client = service.c
+        client = Client(id=None, name=request.name, email=request.email, phone=request.phone)
 
-    except:
+        client_created = service.create_client(client)
+
+        return {
+            "message": "Cliente creado correctamente",
+            "client": client_created
+        }
+
+    except(ValidationException) as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except(AlreadyExistsException) as e:
+        raise HTTPException(status_code=409, detail=str(e))
+
+
+@router.get("/{client_id}")
+def get_client(client_id: int, service = Depends(get_client_service)):
+    
+    try:
+        client = service.get_by_id(client_id)
+        return client
+    except NotFoundException as e:
+        raise HTTPException(status_code=404, detail=str(e))
