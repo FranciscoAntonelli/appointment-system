9b5f1d3834480cdac6dbb7ab1645eb44e47e0e96
Francisco Antonelli <franciscoantonelli1999@gmail.com>
2026-09-09 23:16:06 -0300

    corrijo acento en dia de la semana

--- src/enums/days_of_week.py
+++ src/enums/days_of_week.py
@@ -3,7 +3,7 @@ from enum import Enum
 class DaysOfWeek(Enum):
     LUNES = "Lunes"
     MARTES = "Martes"
-    MIERCOLES = "Miercoles"
+    MIERCOLES = "Miércoles"
     JUEVES = "Jueves"
     VIERNES = "Viernes"
     SABADO = "Sabado"
